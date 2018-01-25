d4eb3d4b8846d7b3e93f8326d7d06570a4b05998
John P Quense <jpquense@gmail.com>
2018-01-25 18:40:35 -0500

    a11y features added

--- youTube-api-test/index.js
+++ youTube-api-test/index.js
@@ -44,7 +44,9 @@ function getDataFromApi(searchTerm, callback) {
 function displayYouTubeSearchData(data) {	
 	// const results = data.items.map((item, index) =>	renderResult(item));
 	const results = data.items.map((item, index) => renderResult(item));
-	
+	const searchNumber = data.items.length;
+	console.log(searchNumber);
+	$('.js-searchNumber').html(`<h3>Your search returned ${searchNumber} search results.</h3>`);
 	$('.js-query-thumbnails').html(results);
 
 	// data.items.forEach(item => $('.js-query-thumbnails').append(renderResult(item)))
@@ -57,14 +59,14 @@ function displayYouTubeSearchData(data) {
 $('.js-query-thumbnails').on('click', '.js-videoFun', function(event) {
 	event.preventDefault();
 	const videoId = $(event.currentTarget).data('videoId');
-	console.log(videoId);
+	// console.log($(event.currentTarget).data('videoId'));
 	$('.js-light-box').html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`)
 });
 
 function renderResult(result) {
 	return `
-		<div>
-			<h2>${result.snippet.title}</h2>
+		<div class="results">
+			<h4>${result.snippet.title}</h4>
 			<a data-videoId="${result.id.videoId}" class="js-videoFun" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
 				<img src="${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}">
 			<a>
